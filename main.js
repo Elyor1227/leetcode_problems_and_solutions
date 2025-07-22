178abf54dc50c4ce5c8b1c10074335f629bf3076
Elyorbek <127598780+Elyor1227@users.noreply.github.com>
2025-07-22 17:22:56 +0500

    Valid sudoku is solved

--- main.js
+++ main.js
@@ -348,3 +348,64 @@ console.log(fourSum([1,0,-1,0,-2,2], 0));
   
     return [start, end];
   }
+
+  // 35. Search Insert Position (easy)
+//   Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2 
+
+// SOLUTION => 
+  var searchInsert = function(nums, target) {
+    let left = 0;
+    let right  = nums.length-1;
+    while(left <= right){
+      let mid = Math.floor((left+right)/2);
+      if(nums[mid] === target)return mid;
+      if(nums[mid] < target){
+        left = mid+1;
+      }else{
+        right = mid-1;
+      }
+    }
+    return left;
+  }
+
+  console.log(searchInsert([1,3,5,6], 5));
+
+  // 36. Valid Sudoku (medium)
+
+//   Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+
+// SOLUTION => 
+  var isValidSudoku = function(board) {
+    const rows = new Array(9).fill(0).map(() => new Array(9).fill(0));
+    const cols = new Array(9).fill(0).map(() => new Array(9).fill(0));
+    const boxes = new Array(9).fill(0).map(() => new Array(9).fill(0));
+
+    for(let i = 0; i< 9; i++){
+      for(let j = 0; j< 9; j++){
+        const num = board[i][j];
+        if(num !== '.'){
+          const boxIndex = Math.floor(i/3)*3 + Math.floor(j/3);
+          if(rows[i][num] || cols[j][num] || boxes[boxIndex][num]) return false;
+          rows[i][num] = true;
+          cols[j][num] = true;
+          boxes[boxIndex][num] = true;
+        }
+      }
+    }
+    return true;
+  }
+
+  console.log(isValidSudoku([["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]));
